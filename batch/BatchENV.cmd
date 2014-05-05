5f11db8bfb06e8c7908082cd8d8eedb1c5fef8e5
kkralove <karel.kralovec@capgemini.com>
2014-05-05 15:41:45 +0200

    Modif

--- batch/BatchENV.cmd
+++ batch/BatchENV.cmd
@@ -1,11 +1,13 @@
 @echo off
 
-set DIR_SPRING=c:\MAVEN_REPOSITORY\org\springframework
+
+set DIR_SPRING=%MAVEN_REPOSITORY%\org\springframework
 
 set JAVA=%JAVA_HOME%\bin\java.exe
 set JAR_ALBUM=%MAVEN_REPOSITORY%\kkr\album\kkr.album\0.0.1\kkr.album-0.0.1.jar
-set JARS_SPRING=%DIR_SPRING%\spring-core\3.2.0.RELEASE\spring-core-3.2.0.RELEASE.jar;%DIR_SPRING%\spring-beans\3.2.0.RELEASE\spring-beans-3.2.0.RELEASE.jar;%DIR_SPRING%\spring-context\3.2.0.RELEASE\spring-context-3.2.0.RELEASE.jar
-set JAR_LOG4J=c:\MAVEN_REPOSITORY\log4j\log4j\1.2.17\log4j-1.2.17.jar
-set CLASSPATH=%JAR_ALBUM%;%JARS_SPRING%;%JAR_LOG4J%
+set JARS_SPRING=%DIR_SPRING%\spring-core\3.2.0.RELEASE\spring-core-3.2.0.RELEASE.jar;%DIR_SPRING%\spring-beans\3.2.0.RELEASE\spring-beans-3.2.0.RELEASE.jar;%DIR_SPRING%\spring-context\3.2.0.RELEASE\spring-context-3.2.0.RELEASE.jar;%DIR_SPRING%\spring-expression\3.2.0.RELEASE\spring-expression-3.2.0.RELEASE.jar 
+set JAR_LOG4J=%MAVEN_REPOSITORY%\log4j\log4j\1.2.17\log4j-1.2.17.jar
+
+set CLASSPATH=%JAR_ALBUM%;%JAR_LOG4J%;%JARS_SPRING%
 
  
