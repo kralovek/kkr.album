91d15c1dd55c83cf3e440447064835e3c6d7ec24
Karel Kralovec <karel.kralovec@gmail.com>
2015-03-16 00:10:54 +0100

    GData

---
+++ src/main/java/kkr/album/components/manager_picasa/ManagerPicasa.java
@@ -0,0 +1,5 @@
+package kkr.album.components.manager_picasa;
+
+public interface ManagerPicasa {
+
+}
---
+++ src/main/java/kkr/album/components/manager_picasa/gdata/ManagerPicasaGData.java
@@ -0,0 +1,7 @@
+package kkr.album.components.manager_picasa.gdata;
+
+import kkr.album.components.manager_picasa.ManagerPicasa;
+
+public class ManagerPicasaGData extends ManagerPicasaGDataFwk implements ManagerPicasa {
+
+}
