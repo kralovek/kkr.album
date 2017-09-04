a65b8f25fcb992cf154c10d5adb0749454c3dad9
Kare Kralovec <karel.kralovec@gmail.com>
2017-09-04 15:52:14 +0200

    moveMiliseconds

--- src/main/java/kkr/album/model/DateNZ.java
+++ src/main/java/kkr/album/model/DateNZ.java
@@ -105,8 +105,7 @@ public class DateNZ implements Comparable<DateNZ>, Cloneable {
 
 	public DateNZ(String date) {
 		if (!date.matches("[0-9]{4}[1-2][0-9][0-3][0-9](_[0-2][0-9][0-5][0-9][0-5][0-9](\\.[0-9]{3})?)?")) {
-			throw new IllegalArgumentException(
-					"Date must be in the format yyyyMMdd, yyyyMMdd_HHmmss or yyyyMMdd_HHmmss.SSS");
+			throw new IllegalArgumentException("Date must be in the format yyyyMMdd, yyyyMMdd_HHmmss or yyyyMMdd_HHmmss.SSS");
 		}
 
 		try {
@@ -126,21 +125,18 @@ public class DateNZ implements Comparable<DateNZ>, Cloneable {
 		}
 
 		if (month > 12) {
-			throw new IllegalArgumentException(
-					"Date must be in the format yyyyMMdd or yyyyMMdd_HHmmss. Bad month: " + month);
+			throw new IllegalArgumentException("Date must be in the format yyyyMMdd or yyyyMMdd_HHmmss. Bad month: " + month);
 		}
 		{
 			int dayM = daysOfMonth(year, month);
 
 			if (day > dayM) {
-				throw new IllegalArgumentException("Date must be in the format yyyyMMdd or yyyyMMdd_HHmmss. Bad day: "
-						+ day + " of month: " + month);
+				throw new IllegalArgumentException("Date must be in the format yyyyMMdd or yyyyMMdd_HHmmss. Bad day: " + day + " of month: " + month);
 			}
 		}
 
 		if (day > 23) {
-			throw new IllegalArgumentException(
-					"Date must be in the format yyyyMMdd or yyyyMMdd_HHmmss. Bad hour: " + hour);
+			throw new IllegalArgumentException("Date must be in the format yyyyMMdd or yyyyMMdd_HHmmss. Bad hour: " + hour);
 		}
 	}
 
@@ -174,20 +170,25 @@ public class DateNZ implements Comparable<DateNZ>, Cloneable {
 
 	public int compareTo(DateNZ dateNZ) {
 		return //
-		year < dateNZ.year ? -1
+		year < dateNZ.year
+				? -1
 				: year > dateNZ.year ? +1 : //
-						month < dateNZ.month ? -1
+						month < dateNZ.month
+								? -1
 								: month > dateNZ.month ? +1 : //
-										day < dateNZ.day ? -1
+										day < dateNZ.day
+												? -1
 												: day > dateNZ.day ? +1 : //
-														hour < dateNZ.hour ? -1 : hour > dateNZ.hour ? +1 : //
-																minute < dateNZ.minute ? -1
-																		: minute > dateNZ.minute ? +1 : //
-																				second < dateNZ.second ? -1
-																						: second > dateNZ.second ? +1 : //
-																								ms < dateNZ.ms ? -1
-																										: ms > dateNZ.ms
-																												? +1 : //
+														hour < dateNZ.hour
+																? -1
+																: hour > dateNZ.hour ? +1 : //
+																		minute < dateNZ.minute
+																				? -1
+																				: minute > dateNZ.minute ? +1 : //
+																						second < dateNZ.second
+																								? -1
+																								: second > dateNZ.second ? +1 : //
+																										ms < dateNZ.ms ? -1 : ms > dateNZ.ms ? +1 : //
 																												0;
 	}
 
@@ -195,11 +196,209 @@ public class DateNZ implements Comparable<DateNZ>, Cloneable {
 		return moveMiliSeconds(moveSeconds * 1000);
 	}
 
-	public DateNZ moveMiliSeconds(int moveMiliSeconds) {
-		if (moveMiliSeconds == 0) {
+	public DateNZ moveMiliSeconds(int moveMiliseconds) {
+		if (moveMiliseconds == 0) {
 			return (DateNZ) clone();
 		}
 
+		//
+		// Miliseconds
+		//
+		int deltaMiliseconds = moveMiliseconds % 1000;
+		int moveSeconds = (moveMiliseconds - moveMiliseconds % 1000) / 1000;
+		int newMilisecond = this.ms + deltaMiliseconds;
+
+		if (newMilisecond > 1000) {
+			newMilisecond -= 1000;
+			moveSeconds++;
+		} else if (newMilisecond < 0) {
+			newMilisecond += 1000;
+			moveSeconds--;
+		}
+
+		//
+		// Seconds
+		//
+		int deltaSeconds = moveSeconds % 60;
+		int moveMinutes = (moveSeconds - moveSeconds % 60) / 60;
+		int newSecond = this.second + deltaSeconds;
+
+		if (newSecond > 60) {
+			newSecond -= 60;
+			moveMinutes++;
+		} else if (newSecond < 0) {
+			newSecond += 60;
+			moveMinutes--;
+		}
+
+		//
+		// Minutes
+		//
+		int deltaMinutes = moveMinutes % 60;
+		int moveHours = (moveMinutes - moveMinutes % 60) / 60;
+		int newMinute = this.minute + deltaMinutes;
+
+		if (newMinute > 60) {
+			newMinute -= 60;
+			moveHours++;
+		} else if (newMinute < 0) {
+			newMinute += 60;
+			moveHours--;
+		}
+
+		//
+		// Hours
+		//
+		int deltaHours = moveHours % 24;
+		int moveDays = (moveHours - moveHours % 24) / 24;
+		int newHour = this.minute + deltaHours;
+
+		if (newHour > 24) {
+			newHour -= 24;
+			moveDays++;
+		} else if (newHour < 0) {
+			newHour += 24;
+			moveDays--;
+		}
+
+		//
+		// Days
+		//
+		int newDay = this.day + moveDays;
+
+		int newYear = this.year;
+		int newMonth = this.month;
+
+		if (moveMiliseconds < 0) {
+			for (; newDay <= 0; newDay += daysOfMonth(newYear, newMonth)) {
+				newMonth--;
+				if (newMonth == 0) {
+					newYear--;
+					if (newYear == 0) {
+						newYear = -1;
+					}
+				}
+			}
+		} else {
+			int currentDaysOfMonth;
+
+			for (; newDay > (currentDaysOfMonth = daysOfMonth(newYear, newMonth)); newDay -= currentDaysOfMonth) {
+				newMonth++;
+				if (newMonth == 13) {
+					newMonth = 1;
+					newYear++;
+				}
+			}
+		}
+		DateNZ retval = new DateNZ(newYear, newMonth, newDay, newHour, newMinute, newSecond, newMilisecond);
+		return retval;
+	}
+
+	/**
+	 * 
+	 * @param dateNZ
+	 * @return
+	 */
+	public long durationMs(DateNZ dateNZ) {
+		int retval = compareTo(dateNZ);
+		if (retval == 0) {
+			return 0;
+		}
+		DateNZ dateMin;
+		DateNZ dateMax;
+		long direction;
+
+		if (retval < 0) {
+			dateMin = this;
+			dateMax = dateNZ;
+			direction = -1;
+		} else {
+			dateMin = dateNZ;
+			dateMax = this;
+			direction = +1;
+		}
+
+		long msDifference;
+
+		if (dateMin.getYear() == dateMax.getYear()) {
+			msDifference = msOfYearCurrent(dateMax) - msOfYearCurrent(dateMin);
+		} else {
+			msDifference = msOfYearCurrent(dateMax) - msOfYearCurrent(dateMin) + msOfYear(dateMin.getYear());
+			for (int i = dateMin.getYear() + 1; i < dateMax.getYear(); i++) {
+				msDifference += msOfYear(i);
+			}
+		}
+
+		return direction * msDifference;
+	}
+
+	private static long msOfYear(int year) {
+		return isYear29(year) ? MS_YEAR_29 : MS_YEAR_28;
+	}
+
+	private static boolean isYear29(int year) {
+		return year % 500 == 0 ? true : year % 100 == 0 ? false : year % 4 == 0;
+	}
+
+	private static long msOfYearCurrent(DateNZ date) {
+		long ms = 0;
+		for (int i = 1; i < date.getMonth(); i++) {
+			int days = daysOfMonth(date.getYear(), i);
+			ms += days * MS_DAY;
+		}
+		ms += (date.getDay() - 1) * MS_DAY + date.getHour() * 60 * 60 * 1000 + date.getMinute() * 60 * 1000 + date.getSecond() * 1000 + date.getMs();
+		return ms;
+	}
+
+	private static int daysOfMonth(int year, int month) {
+		int dayM;
+		switch (month) {
+			case 4 :
+			case 6 :
+			case 9 :
+			case 11 : {
+				dayM = 30;
+				break;
+			}
+			case 2 : {
+				dayM = isYear29(year) ? 29 : 28;
+				break;
+			}
+			default :
+				dayM = 31;
+		}
+		return dayM;
+	}
+
+	protected Object clone() {
+		DateNZ retval = new DateNZ(year, month, day, hour, minute, second, ms);
+		return retval;
+	}
+
+	public Date toDate() {
+		Calendar calendar = new GregorianCalendar(UtilsPattern.GMT);
+		calendar.set(year, month - 1, day, hour, minute, second);
+		calendar.set(Calendar.MILLISECOND, ms);
+		Date date = calendar.getTime();
+		return date;
+	}
+
+	public String toString(String pattern) {
+		DateFormat dateFormat = new SimpleDateFormat(pattern);
+		dateFormat.setTimeZone(UtilsPattern.GMT);
+		Date date = toDate();
+		return dateFormat.format(date);
+	}
+
+	public int hashCode() {
+		return (((((year * 12 + month) * 31 + day) * 24 + hour) * 60 + minute) * 60 + second) * 1000 * ms;
+	}
+
+	public String toString() {
+		return String.format("%04d/%02d/%02d %02d:%02d:%02d.%03d", year, month, day, hour, minute, second, ms);
+	}
+
+	private DateNZ f(int moveMiliSeconds) {
 		if (moveMiliSeconds < 0) {
 			int newMiliSeconds = this.ms + moveMiliSeconds;
 			int ms = newMiliSeconds < 0 ? 1000 + newMiliSeconds % 1000 : newMiliSeconds;
@@ -272,113 +471,12 @@ public class DateNZ implements Comparable<DateNZ>, Cloneable {
 		}
 	}
 
-	/**
-	 * 
-	 * @param dateNZ
-	 * @return
-	 */
-	public long durationMs(DateNZ dateNZ) {
-		int retval = compareTo(dateNZ);
-		if (retval == 0) {
-			return 0;
-		}
-		DateNZ dateMin;
-		DateNZ dateMax;
-		long direction;
-
-		if (retval < 0) {
-			dateMin = this;
-			dateMax = dateNZ;
-			direction = -1;
-		} else {
-			dateMin = dateNZ;
-			dateMax = this;
-			direction = +1;
-		}
-
-		long msDifference;
-
-		if (dateMin.getYear() == dateMax.getYear()) {
-			msDifference = msOfYearCurrent(dateMax) - msOfYearCurrent(dateMin);
-		} else {
-			msDifference = msOfYearCurrent(dateMax) - msOfYearCurrent(dateMin) + msOfYear(dateMin.getYear());
-			for (int i = dateMin.getYear() + 1; i < dateMax.getYear(); i++) {
-				msDifference += msOfYear(i);
-			}
-		}
-
-		return direction * msDifference;
-	}
-
-	private static long msOfYear(int year) {
-		return isYear29(year) ? MS_YEAR_29 : MS_YEAR_28;
-	}
-
-	private static boolean isYear29(int year) {
-		return year % 500 == 0 ? true : year % 100 == 0 ? false : year % 4 == 0;
-	}
-
-	private static long msOfYearCurrent(DateNZ date) {
-		long ms = 0;
-		for (int i = 1; i < date.getMonth(); i++) {
-			int days = daysOfMonth(date.getYear(), i);
-			ms += days * MS_DAY;
-		}
-		ms += (date.getDay() - 1) * MS_DAY + date.getHour() * 60 * 60 * 1000 + date.getMinute() * 60 * 1000
-				+ date.getSecond() * 1000 + date.getMs();
-		return ms;
-	}
-
-	private static int daysOfMonth(int year, int month) {
-		int dayM;
-		switch (month) {
-		case 4:
-		case 6:
-		case 9:
-		case 11: {
-			dayM = 30;
-			break;
-		}
-		case 2: {
-			dayM = isYear29(year) ? 29 : 28;
-			break;
-		}
-		default:
-			dayM = 31;
-		}
-		return dayM;
-	}
-
-	protected Object clone() {
-		DateNZ retval = new DateNZ(year, month, day, hour, minute, second, ms);
-		return retval;
-	}
-
-	public Date toDate() {
-		Calendar calendar = new GregorianCalendar(UtilsPattern.GMT);
-		calendar.set(year, month - 1, day, hour, minute, second);
-		calendar.set(Calendar.MILLISECOND, ms);
-		Date date = calendar.getTime();
-		return date;
-	}
-
-	public String toString(String pattern) {
-		DateFormat dateFormat = new SimpleDateFormat(pattern);
-		dateFormat.setTimeZone(UtilsPattern.GMT);
-		Date date = toDate();
-		return dateFormat.format(date);
-	}
-
-	public int hashCode() {
-		return (((((year * 12 + month) * 31 + day) * 24 + hour) * 60 + minute) * 60 + second) * 1000 * ms;
-	}
-
-	public String toString() {
-		return String.format("%04d/%02d/%02d %02d:%02d:%02d.%03d", year, month, day, hour, minute, second, ms);
-	}
-
 	public static void main(String[] argv) {
-		DateNZ dateNZ = new DateNZ("20150607 134512.345", "yyyyMMdd HHmmss.SSS");
-		dateNZ = new DateNZ();
+		DateNZ dateNZ = new DateNZ("20170902 020315.012", "yyyyMMdd HHmmss.SSS");
+
+		DateNZ dateNZmoved = dateNZ.moveMiliSeconds(-390015);
+
+		System.out.println("BEFORE: " + dateNZ);
+		System.out.println("AFTER:  " + dateNZmoved);
 	}
 }
